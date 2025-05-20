c05a366567415142455c66e13863d9c3cbb7d13c
kojirocl <kojirocl@users.noreply.github.com>
2025-05-19 22:37:47 -0400

    Elimina carpetas ignoradas del seguimiento

--- tmp/j6wc2ffcgezd.382sbvk7bgw0c.php
+++
@@ -1,5 +0,0 @@
-
-	<option selected>Todas</option>
-	<?php foreach (($ciudades?:[]) as $ciudad): ?>
-		<option value="<?= ($ciudad['city']) ?>"><?= ($ciudad['city']) ?></option>	
-	<?php endforeach; ?>
--- tmp/j6wc2ffcgezd.g5p6vx3j8ku9.php
+++
@@ -1,4 +0,0 @@
-<div class="<?= ($mensaje['clases']) ?>" role="alert">
-    <i class="bi <?= ($mensaje['icono']) ?>"></i> <?= ($mensaje['contenido'])."
-" ?>
-</div> 
